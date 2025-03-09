1f42ddd6b9376b4aab02d4032ec3e4932fea4df4
Anubhav-1305 <roy.anubhav1305@gmail.com>
2025-03-10 02:58:50 +0530

    Update EmployeeRequestDTO.java

--- src/main/java/com/example/critter/DTO/EmployeeRequestDTO.java
+++ src/main/java/com/example/critter/DTO/EmployeeRequestDTO.java
@@ -5,10 +5,6 @@ import com.example.critter.Entity.EmployeeSkill;
 import java.time.LocalDate;
 import java.util.Set;
 
-/**
- * Represents a request to find available employees by skills. Does not map
- * to the database directly.
- */
 public class EmployeeRequestDTO {
     private Set<EmployeeSkill> skills;
     private LocalDate date;
